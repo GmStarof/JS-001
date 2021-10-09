7afe8427fcd8e0e85cd3c60e873d9635281e8418
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-09 00:08:24 -0300

    Update script.js

--- Exemplos de JS/Modelo 01/script.js
+++ Exemplos de JS/Modelo 01/script.js
@@ -1,21 +1,24 @@
-function carregar(){
+function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('fotodia')
 var data = new Date()
-//var hora = data.getHours()
-var hora = 19
+var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
-if( hora >= 0 && < 12) {
+if( hora >= 0 && < 12){
     //BOM DIA!
-    img.src = 'Manhã.png'
+    img.scr = 'Manhã.png'
+    document.body.style.background= '#f2caac'
 }
-else if(hora >= 12 && < 18 ) {
+else if(hora >= 12 <= 18){
     //BOA TARDE!
     img.src = 'Tarde.png'
+    document.body.style.background= '#e46600'
 }
 else{
-    //BOA NOITE
-    img.src= 'Noite.png'
+    //BOA NOITE!
+    img.src = 'Noite.png'
+    document.body.style.background= '#2b4449'
+}
 }
 
 }

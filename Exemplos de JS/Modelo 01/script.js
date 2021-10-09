797657361e513a31da05d6dacbf8888e5aec2b53
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-09 00:14:23 -0300

    .

--- Exemplos de JS/Modelo 01/script.js
+++ Exemplos de JS/Modelo 01/script.js
@@ -4,12 +4,12 @@ var img = window.document.getElementById('fotodia')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
-if( hora >= 0 && < 12){
+if( hora >= 0 && hora < 12){
     //BOM DIA!
     img.scr = 'Manhã.png'
     document.body.style.background= '#f2caac'
 }
-else if(hora >= 12 <= 18){
+else if(hora >= 12 && hora <= 18){
     //BOA TARDE!
     img.src = 'Tarde.png'
     document.body.style.background= '#e46600'
@@ -19,6 +19,4 @@ else{
     img.src = 'Noite.png'
     document.body.style.background= '#2b4449'
 }
-}
-
 }

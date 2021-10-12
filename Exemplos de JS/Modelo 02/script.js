d6b931fefcdf65af6f8f326593f3b1305c19ac1a
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-12 15:27:26 -0300

    .

--- Exemplos de JS/Modelo 02/script.js
+++ Exemplos de JS/Modelo 02/script.js
@@ -18,33 +18,42 @@ if(fsex[0].checked){
     gênero =  'Homem'
     if ( idade >= 0 && idade < 10){
     //Criança
+    img.setAttribute('src','foto-homem criança.png')
 }
     else if( idade < 21){
     //Jovem
+    img.setAttribute('src','foto-homem jovem.png')
 }
     else if(idade < 50){
     //adulto
+    img.setAttribute('src', 'foto-homem adulto.png')
 }
     else if( idade >50){
     //Idoso
+    img.setAttribute('src','foto-homem idoso.png')
 }
 }
 else{
     gênero = 'Mulher'
     if ( idade >= 0 && idade < 10){
         //Criança
+        img.setAttribute('src', 'foto-mulher criança.png')
     }
         else if( idade < 21){
         //Jovem
+        img.setAttribute('src','foto-mulher jovem.png')
     }
         else if(idade < 50){
         //adulta
+        img.setAttribute('src','foto-mulher adulta.png')
     }
         else if( idade >50){
         //Idosa
+        img.setAttribute('src','foto-mulher idosa.png')
     }
 }
 res.style.textAlign= 'center'
 res.innerHTML = `Detectamos, ${gênero} com ${idade} anos.`
+res.appendChild(img)
 }
 }

2f105ee59c35577a5b8c0590be2bd29917ce7736
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-12 15:38:58 -0300

    Update script.js

--- Exemplos de JS/Modelo 02/script.js
+++ Exemplos de JS/Modelo 02/script.js
@@ -5,7 +5,7 @@ var ano = data.getFullYear()
 var fano = window.document.getElementById('txtano')
 var res = window.document.getElementById('res')
 
-if (fano.value.length == 0 || fano.value > ano )
+if (fano.value.length == 0 || fano.value > ano || fano.value < 1900)
     window.alert('verifique os dados e tente novavemente!')
 
 else{
@@ -17,38 +17,38 @@ else{
 if(fsex[0].checked){
     gênero =  'Homem'
     if ( idade >= 0 && idade < 10){
-    //Criança
+    gênero = 'Criança'
     img.setAttribute('src','foto-homem criança.png')
 }
     else if( idade < 21){
-    //Jovem
+    gênero = 'Jovem'
     img.setAttribute('src','foto-homem jovem.png')
 }
     else if(idade < 50){
-    //adulto
+    gênero = 'Adulto'
     img.setAttribute('src', 'foto-homem adulto.png')
 }
     else if( idade >50){
-    //Idoso
+    gênero = 'Idoso'
     img.setAttribute('src','foto-homem idoso.png')
 }
 }
 else{
     gênero = 'Mulher'
     if ( idade >= 0 && idade < 10){
-        //Criança
+        gênero = 'Criança'
         img.setAttribute('src', 'foto-mulher criança.png')
     }
         else if( idade < 21){
-        //Jovem
+        gênero = 'Jovem'
         img.setAttribute('src','foto-mulher jovem.png')
     }
         else if(idade < 50){
-        //adulta
+        gênero = 'Adulta'
         img.setAttribute('src','foto-mulher adulta.png')
     }
         else if( idade >50){
-        //Idosa
+        gênero = 'Idosa'
         img.setAttribute('src','foto-mulher idosa.png')
     }
 }

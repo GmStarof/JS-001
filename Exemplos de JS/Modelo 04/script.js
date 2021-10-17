44fc2ba34be9a9579af1cd8ea3b0e8efc61a9ff3
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-16 21:51:29 -0300

    .

--- Exemplos de JS/Modelo 04/script.js
+++ Exemplos de JS/Modelo 04/script.js
@@ -1,1 +1,18 @@
 alert('Olá!')
+function gerar(){
+    var n = window.document.getElementById('txtnum')
+    var tab = window.document.getElementById('seltab')
+    if(n.value.length == 0){ 
+        window.alert('Por favor, Digite um numero.')
+      
+    }else{
+        tab.innerHTML = '' 
+        var num = Number(n.value)
+        for(var c = 1; c <= 10; c++ ){
+            var item = window.document.createElement('option')
+            item.text = `${num} x ${c} = ${c*num}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}

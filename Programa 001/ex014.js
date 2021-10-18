bd268c54b2931f22e6c77ea2a6c3e66973018857
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-18 15:49:50 -0300

    Update ex014.js

--- Programa 001/ex014.js
+++ Programa 001/ex014.js
@@ -1,8 +1,12 @@
 var num = [ 1, 2, 3, ]
+var num1 = 2
 num.length
 num[3] = 4
 num.push(5)
 console.log(num)
-for(var c = 1; c + num; c++){
-    console.log(c)
+for(var c = 0; c < num.length; c++){
+    console.log(`${c} ${num[c] }`)
+}
+for( num1 in num){
+    console.log(`${num1} ${num[num1]}`)
 }

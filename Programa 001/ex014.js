a3c62535136460c21cc2e6f0ef0a6a83eabab61c
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-18 16:02:07 -0300

    Update ex014.js

--- Programa 001/ex014.js
+++ Programa 001/ex014.js
@@ -1,5 +1,6 @@
 var num = [ 1, 2, 3, ]
 var num1 = 2
+var num2 = num.indexOf(4)
 num.length
 num[3] = 4
 num.push(5)
@@ -9,4 +10,9 @@ for(var c = 0; c < num.length; c++){
 }
 for( num1 in num){
     console.log(`${num1} ${num[num1]}`)
+}
+if( num2 == -1){
+    console.log(`O valor não foi encontrado.`)
+}else{
+    console.log(`${num2}`)
 }

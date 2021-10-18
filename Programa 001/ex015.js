10e8184a0c64df5e1068562820ea1d650d2cb4bf
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-18 16:40:22 -0300

    Create ex015.js

---
+++ Programa 001/ex015.js
@@ -0,0 +1,13 @@
+function parimp(num){
+    if(num%2 ==0){
+    return 'Par!'
+}else{
+    return 'Impar!'
+}
+}
+console.log(parimp(2))
+
+function soma(n1,n2){
+   return n1 +n2
+}
+console.log( 2 + 4 )

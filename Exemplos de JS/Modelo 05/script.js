04da25a34f71f0205571d657a27a91ff935b18a1
Guilherme Mateus <guimateus.csilva@gmail.com>
2021-10-20 15:00:46 -0300

    .

--- Exemplos de JS/Modelo 05/script.js
+++ Exemplos de JS/Modelo 05/script.js
@@ -1,1 +1,35 @@
 alert('Olá!')
+
+var num = document.getElementById('numero')
+var list = document.getElementById(list)
+var res = document.getElementById('res')
+var valores = []
+
+
+function fnum(n){
+    if( Number(n) >= 1 && Number(n) <= 100){
+    return true
+}else{
+    return false
+    }
+}
+
+
+
+function flist(n, 1){
+    if(l.indexOf(Number(n)) != -1){
+    return true
+}else{
+    return false
+    }
+}
+
+
+
+function adicionar(){
+if(fnum(num.value) && flist(num.value, valores) ){
+
+}else{
+    window.alert('Valor invalido ou já encontrado na lista!')
+}
+}
